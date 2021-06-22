f899e091fd904f4c7ec2a331db792d1e41de9674
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-06-22 14:38:18 -0400

    initial commit, creates client and server directories with boilerplate code, installs dependencies for both client/server side, starts development production for memories-project

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+// import routes
+import postRoutes from "./routes/posts.js";
+
+// initialize express application
+const app = express();
+
+// express middleware to connect postRoutes to application
+app.use("/posts", postRoutes);
+
+// body-parser set up
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+// initiliaze CORS
+app.use(cors());
+
+// connect server with Mongo Database
+const CONNECTION_URL =
+  "mongodb+srv://mattyricks:mattyricks123@cluster0.o7fa8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+// connect mongoose to mongo and server
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+mongoose.set("useFindAndModify", false);
---
+++ server/models/postMessage.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+// what is a Schema?
+// Mongoose allows us to give uniformity to our documents
+// specify properties that each post will have
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;
