fd30e2555d195f04a8c738b8ede13b1a44bc9778
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-06-23 17:37:14 -0400

    begins preparing sign up / sign in features for the user's manual login process by utilizing intial state, useState and eventHandlers, creates authentication action types to be used to effectively sign up / sign in users and redirect them to the home page of the application

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -13,22 +13,42 @@ import { useHistory } from "react-router-dom";
 import Icon from "./icon";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Input from "./Input";
+import { signin, signup } from "../../actions/auth";
 
 import useStyles from "./styles";
+
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const Auth = () => {
   const classes = useStyles();
   const [showPassword, setShowPassword] = useState(false);
   const [isSignup, setIsSignup] = useState(false);
+  const [formData, setFormData] = useState(initialState);
   const dispatch = useDispatch();
   const history = useHistory();
 
   const handleShowPassword = () =>
     setShowPassword((prevShowPassword) => !prevShowPassword);
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-  const handleChange = () => {};
+    if (isSignup) {
+      dispatch(signup(formData, history));
+    } else {
+      dispatch(signin(formData, history));
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
   const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup);
