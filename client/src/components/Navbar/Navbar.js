99929885a91abdaf0236bec732cb14960a3beba5
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-06-24 12:49:16 -0400

    adds feature to check the expiration of the JWT token and if the  token is expired the user will be logged out, prepares for deployment via heroku and netlify

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Avatar, Typography, Toolbar, Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 import useStyles from "./styles";
 import memories from "../../images/memories.png";
 
@@ -24,6 +25,13 @@ const Navbar = () => {
     const token = user?.token;
 
     // JWT....
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        logout();
+      }
+    }
 
     setUser(JSON.parse(localStorage.getItem("profile")));
   }, [location]);
