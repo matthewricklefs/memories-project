fc64a7e60f674bee5a4b256431b8b8ac71b212bf
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-06-30 16:49:54 -0400

    creates Pagination components, implements useQuery, useHistory, useStyles, and useState hooks inside of the Home components, adds button, chip input, textfield, and appbar to create a search posts and search tag option for the users

---
+++ client/src/components/PostDetails/PostDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostDetails = () => {
+  console.log("post details ");
+  return (
+    <div>
+      <h1>Posts</h1>
+    </div>
+  );
+};
+
+export default PostDetails;
